0728195db09717b364f5b3a89ffa7237e9da5dc2
EnricoGiovanno8 <genrico189@gmail.com>
2022-02-02 15:36:59 +0700

    Create and Get Cookie Done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,28 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './models/login.dto';
+import { RegisterDto } from './models/register.dto';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AuthController {
+  constructor(private userService: UserService) {}
+
   @Post('register')
-  async register(@Body() body) {
-    return await body;
+  async register(@Body() body: RegisterDto) {
+    return await this.userService.register(body);
+  }
+
+  @Post('login')
+  async login(
+    @Body() body: LoginDto,
+    @Res() response: Response,
+  ) {
+    return await this.userService.login(body, response);
+  }
+
+  @Get('user')
+  async getUserByCookie(@Req() request: Request) {
+      return await this.userService.getUserByCookie(request)
   }
 }
