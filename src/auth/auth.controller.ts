0364c77d4ef5d43ec134ac8f341e064958a83c4f
EnricoGiovanno8 <genrico189@gmail.com>
2022-02-02 16:28:30 +0700

    Use Guard for Cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,17 +6,23 @@ import {
   Post,
   Req,
   Res,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { LoginDto } from './models/login.dto';
 import { RegisterDto } from './models/register.dto';
 import { Request, Response } from 'express';
+import { AuthGuard } from './auth.guard';
+import { JwtService } from '@nestjs/jwt';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller()
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   @Post('register')
   async register(@Body() body: RegisterDto) {
@@ -31,13 +37,15 @@ export class AuthController {
     return await this.userService.login(body, response);
   }
 
+  @UseGuards(AuthGuard)
   @Get('user')
   async user(@Req() request: Request) {
     return await this.userService.getUserByCookie(request);
   }
 
+  @UseGuards(AuthGuard)
   @Post('logout')
   async logout(@Res({ passthrough: true }) response: Response) {
-      return await this.userService.logout(response)
+    return await this.userService.logout(response);
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule} from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './models/user.entity';
 import { UserController } from './user.controller';
